72027a172d7541f313c6963e30bda6063cb8317e
irek02 <imirgaleev@gmail.com>
2019-09-20 14:57:08 -0700

    add service to get mocked data with an interface and update the component to use the service to fetch the data

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homes',
-  templateUrl: './homes.component.html',
-  styleUrls: ['./homes.component.less']
+  templateUrl: './homes.component.html'
 })
 export class HomesComponent implements OnInit {
 
-  constructor() { }
+  homes = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
