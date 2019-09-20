709400d5de47823876976ff2d4f253a7c550055f
irek02 <imirgaleev@gmail.com>
2019-09-19 19:08:21 -0700

    added a route; added uikit; added homes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomesComponent } from './homes/homes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homes',
+    component: HomesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomesComponent } from './homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule,
