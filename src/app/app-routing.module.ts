96889ffcaf81d64af8194d9de1301880e3bafd75
irek02 <imirgaleev@gmail.com>
2019-09-23 15:03:38 -0700

    add route and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomesComponent } from './homes/homes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homes',
+    component: HomesComponent
+  },
+  {
+    path: '',
+    redirectTo: 'homes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/homes/homes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homes',
+  templateUrl: './homes.component.html'
+})
+export class HomesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
