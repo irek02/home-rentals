f26ba5fa0f3d10316939fdce535aff2f4a384b58
irek02 <imirgaleev@gmail.com>
2019-09-22 18:41:37 -0700

    init

---
+++ src/app/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHomes() {
+
+    return this.httpClient.get('assets/homes.json')
+      .pipe(
+        delay(2000)
+      );
+
+  }
+}
---
+++ src/app/homes/homes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-homes',
+  templateUrl: './homes.component.html'
+})
+export class HomesComponent implements OnInit {
+
+  homes$ = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+  }
+
+}
