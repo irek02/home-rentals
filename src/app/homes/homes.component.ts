4ee45d4b9b0d96cb3f32ac69121a42e5f41a831b
irek02 <imirgaleev@gmail.com>
2019-09-24 17:08:52 -0700

    add service and show data in the component

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homes',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomesComponent implements OnInit {
 
-  constructor() { }
+  homes$ = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
