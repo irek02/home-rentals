372c2be9637cf703d0d59ed50b92252c45aaee42
irek02 <imirgaleev@gmail.com>
2019-11-19 17:56:31 -0800

    add search

---
+++ src/app/home-search/home-search.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-search',
+  templateUrl: './home-search.component.html'
+})
+export class HomeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
